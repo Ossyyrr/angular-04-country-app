939e9a07cd6dd5dd13523e9098a73a02f265c080
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-06 10:34:46 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CountriesModule } from './countries/countries.module';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
