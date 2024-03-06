0aba92d35c3625712bae8f7e2e98547454ab91db
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-06 15:06:42 +0100

    wip

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { catchError, of } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styleUrl: './country-page.component.css'
+  styleUrl: './country-page.component.css',
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService
+  ) {}
 
+  public country?: Country;
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+      this.searchCountryById(id);
+    });
+  }
+
+  searchCountryById(id: string) {
+    this.countriesService.searchCountryById(id).subscribe((country) => {
+      if (!country) {
+        this.router.navigateByUrl('');
+        return;
+      }
+      this.country = country;
+    });
+  }
 }
